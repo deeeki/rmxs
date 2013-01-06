ba383484f0800027043cb724c43ec4750410dcb6
itzki <itzki.h@gmail.com>
2013-01-06 21:31:59 +0900

    Implement prfm_rmx

---
+++ app/prfm_rmx.rb
@@ -0,0 +1,28 @@
+# coding: utf-8
+require File.expand_path('../../config/prfm_rmx', __FILE__)
+
+SITES = ['Youtube', 'Nicovideo', 'Soundcloud', 'Mixcloud']
+
+hour = Time.now.hour
+site = SITES[hour % 4]
+keyword = (hour < 12)? 'Perfume mix' : 'Perfume rmx'
+klass = Bremen.const_get(site)
+
+uids = Wordpress::Xmlrpc.get_uids(klass.name.split('::').last)
+
+tracks = klass.search(keyword: keyword, limit: 50)
+exit if tracks.empty?
+
+tracks.reverse.each do |track|
+  next if uids.include?(track.uid.to_s)
+
+  post = track.to_wordpress_post
+  id = Wordpress::Xmlrpc.new_post(post)
+
+  unless track.deniable?
+    status = track.to_twitter_status
+    Twitter.update(status)
+  end
+
+  break
+end
---
+++ config/prfm_rmx.rb
@@ -0,0 +1,26 @@
+require 'bundler/setup'
+Bundler.require(:default) if defined?(Bundler)
+$:.unshift(File.expand_path('../../lib', __FILE__))
+
+require 'yaml'
+require 'bremen/ext'
+require 'wordpress'
+@config = YAML.load_file(File.expand_path('../../config/prfm_rmx/application.yml', __FILE__))
+
+Wordpress.configure do |config|
+  config.url = @config[:wordpress][:url]
+  config.blog_id = @config[:wordpress][:blog_id]
+  config.username = @config[:wordpress][:username]
+  config.password = @config[:wordpress][:password]
+end
+Twitter.configure do |config|
+  config.consumer_key = @config[:twitter][:consumer_key]
+  config.consumer_secret = @config[:twitter][:consumer_secret]
+  config.oauth_token = @config[:twitter][:access_token]
+  config.oauth_token_secret = @config[:twitter][:access_token_secret]
+end
+Bremen::Soundcloud.client_id = @config[:soundcloud][:client_id]
+Bremen::Validator.configure do |config|
+  config.whitelist_file = File.expand_path('../../config/prfm_rmx/WHITELIST', __FILE__)
+  config.blacklist_file = File.expand_path('../../config/prfm_rmx/BLACKLIST', __FILE__)
+end
