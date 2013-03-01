e2b9311f3188db34242077fe4cb059f6df2dd749
itzki <itzki.h@gmail.com>
2013-03-01 20:31:05 +0900

    Implement ystk_rmx

---
+++ app/ystk_rmx.rb
@@ -0,0 +1,30 @@
+# coding: utf-8
+APP_NAME = 'ystk_rmx'
+
+require File.expand_path('../../config/application', __FILE__)
+
+SITES = ['Youtube', 'Nicovideo', 'Soundcloud', 'Mixcloud']
+
+hour = Time.now.hour
+site = SITES[hour % 4]
+keyword = (hour < 12)? 'ヤスタカ mix' : 'capsule mix'
+klass = Bremen.const_get(site)
+
+uids = Wordpress::Xmlrpc.get_uids(site)
+
+tracks = klass.search(keyword: keyword, limit: 50)
+exit if tracks.empty?
+
+tracks.reverse.each do |track|
+  next if uids.include?(track.uid.to_s)
+
+  post = track.to_wordpress_post
+  id = Wordpress::Xmlrpc.new_post(post)
+
+  if !track.deniable? && (track.allowable? || site == 'Nicovideo')
+    status = track.to_twitter_status(hashtag: '#ystk')
+    Twitter.update(status)
+  end
+
+  break
+end
